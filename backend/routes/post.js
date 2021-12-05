f88494dbf76be02add2713109f24fa3902d72b3f
fadi shahad <sarlsft.78@gmail.com>
2021-12-05 17:20:25 +0100

    nouvelle modification

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,16 +1,25 @@
-module.exports = (app) => {
-  const posts = require("../controllers/post");
-  const auth = require("../middleware/auth");
+module.exports = app => {
+    const posts = require('../controllers/post');
+    const auth = require('../middleware/auth');
 
-  const router = require("express").Router();
-  const multer = require("../middleware/multer-config");
+    const router = require('express').Router();
+    const multer = require('../middleware/multer-config');
 
-  router.post("/", auth, multer, posts.createPost);
-  router.get("/:id", auth, posts.getOnePost);
-  router.get("/users/:id", auth, posts.getAllUsersPosts);
-  router.get("/", auth, posts.getAllPosts);
-  router.put("/:id", auth, multer, posts.modifyPost);
-  router.delete("/:id", auth, posts.deletePost);
+    // Post CRUD
+    router.post('/', auth, multer, posts.createPost);
+    router.get('/:id', auth, posts.getOnePost);
+    router.get('/users/:id', auth, posts.getAllUsersPosts);
+    router.get('/', auth, posts.getAllPosts);
+    router.put('/:id', auth, multer, posts.modifyPost);
+    router.delete('/:id', auth, posts.deletePost);
 
-  app.use("/api/posts", router);
+    // Images
+    router.get('/image/:id', auth, postCtrl.getOneImage);
+
+    // Like / Unlike
+    router.patch('/:id/likeunlike', auth, postCtrl.likeUnlikePost);
+    router.post('/:id/postLikedByUser', auth, postCtrl.postLikedByUser);
+    router.post('/:id/likeunlike', auth, postCtrl.countLikes);
+
+    app.use('/api/posts', router);
 };
